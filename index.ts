8f80da960b026d64d56227d1dc15f9e0679c8d6a
Anmol Sudhir <anmolsudhir.2001@gmail.com>
2024-04-24 20:10:05 +0530

    feat: Added event listeners

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ TODO: Scaling and load balancing with redis
 */
 import { createServer } from "http";
 import SocketService from "./src/socketio";
+import { instrument } from "@socket.io/admin-ui";
 
 const PORT = process.env.PORT ?? 8080
 const httpServer = createServer();
@@ -10,5 +11,7 @@ const httpServer = createServer();
 const socketio = new SocketService();
 socketio.io.attach(httpServer)
 socketio.init()
+
+instrument(socketio.io, { auth: false })
 
 httpServer.listen(PORT, () => console.log(`Server listening on ${PORT}`));
